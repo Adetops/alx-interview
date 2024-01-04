a553350e1b58d5767a12dc10f67187f8935eb32f
Adetops <heritagediwamu@gmail.com>
2024-01-04 23:11:33 +0100

    Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/bin/bash/env python3
+''' Defines the function pascal_triangle(n) '''
+
+def pascal_triangle(n):
+    ''' returns a list of integers representing the Pascal's triangle of n '''
+    if n <= 0:
+        return []
+    else:
+        pascalTriangle = []
+        for x in range(n):
+            row = [1]
+            if pascalTriangle:
+                last_row = pascalTriangle[-1]
+                row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+                row.append(1)
+            pascalTriangle.append(row)
+        return pascalTriangle            
