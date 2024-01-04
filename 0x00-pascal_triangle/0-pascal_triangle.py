7371d0c757e9f4519cd9298fb12cf8de8f9688eb
Adetops <heritagediwamu@gmail.com>
2024-01-04 23:27:10 +0100

    two blank lines

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' Defines the function pascal_triangle(n) '''
 
+
 def pascal_triangle(n):
     ''' returns a list of integers representing the Pascal's triangle of n '''
     if n <= 0:
