723da98e6f3973b6a3c750288ad7a53bb43cec32
Adetops <heritagediwamu@gmail.com>
2024-02-24 21:46:49 +0100

    modified

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,42 @@
 #!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie
- * The first positional argument passed is the Movie ID
- * Display one character name per line in the same order
- * as  list in the /films/ endpoint
- */
 
 const request = require('request');
-const filmNum = process.argv[2];
+
+const movieId = process.argv[2];
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
-// Makes API request, sets async to allow await promise
-request(filmURL + filmNum, async (err, res, body) => {
-  if (err) return console.error(err);
 
-  // find URLs of each character in the film as a list obj
-  const charURLList = JSON.parse(body).characters;
+// Make API request
+request(filmURL + movieId, async (err, res, body) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
 
-  // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
-  for (const charURL of charURLList) {
-    await new Promise((resolve, reject) => {
-      request(charURL, (err, res, body) => {
-        if (err) return console.error(err);
+  try {
+    // Parse response body
+    const filmData = JSON.parse(body);
 
-        // finds each character name and prints in URL order
-        console.log(JSON.parse(body).name);
-        resolve();
+    // Extract character URLs
+    const charURLList = filmData.characters;
+
+    // Iterate over character URLs and make requests
+    for (const charURL of charURLList) {
+      await new Promise((resolve, reject) => {
+        request(charURL, (err, res, body) => {
+          if (err) {
+            console.error('Error:', err);
+            reject(err);
+            return;
+          }
+
+          // Parse character data and print name
+          const charData = JSON.parse(body);
+          console.log(charData.name);
+          resolve();
+        });
       });
-    });
+    }
+  } catch (error) {
+    console.error('Error:', error);
   }
 });
